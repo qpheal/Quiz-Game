335d75124d769b38ed8454a014ff1ee5b380b87e
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-19 01:03:15 +0000

    Update

--- quiz.js
+++ quiz.js
@@ -13,33 +13,40 @@ const prompt = require("prompt-sync")()
 
 console.log("Welcome to the coding Quiz!")
 
+// Keeping Track of correct answers
+let rightAnswers = 0;
+
+//First Question
 const answer1 = prompt("What is my favourite food? ")
 const correctAnswer = "Kenkey"
 
 // Note not all users may enter right answer in same case as you
 // So use  method to convert thier case to be like yours 
 if (answer1[0].toUpperCase() + answer1.slice(1).toLowerCase() === correctAnswer){
-    console.log("You got it right")
+    console.log("You got it right");
+    rightAnswers +=1
 } else console.log(" Boboooooooooooo")
 
+//Question 2
 const answer2 = prompt("Who is my bestie? ")
 const correctAnswer2 = "Carl"
 
-// Note not all users may enter right answer in same case as you
-// So use  method to convert thier case to be like yours 
 if (answer2[0].toUpperCase() + answer2.slice(1).toLowerCase() === correctAnswer2){
-    console.log("You got it right")
+    console.log("You got it right");
+    rightAnswers++
 } else console.log(" Boboooooooooooo")
 
+//Question 3
 const answer3 = prompt("What is my favourite book? ")
 const correctAnswer3 = "Bible"
 
-// Note not all users may enter right answer in same case as you
-// So use  method to convert thier case to be like yours 
 if (answer3[0].toUpperCase() + answer3.slice(1).toLowerCase() === correctAnswer3){
-    console.log("You got it right")
+    console.log("You got it right");
+    rightAnswers++
 } else console.log(" Boboooooooooooo")
 
+// Printing number of rightly answer Question
+console.log(`You got  ${rightAnswers} questions correct` )
 
 
 
